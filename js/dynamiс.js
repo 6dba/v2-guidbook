61ac28cebefbdb2cfcf61088a94c0e3db132b911
ArhaTe <vova.kamashev_05@mail.ru>
2022-10-16 12:38:00 +0700

    sort/refactor dynamic

--- js/dynamiс.js
+++ js/dynamiс.js
@@ -7,54 +7,45 @@ let end = false;
 let number = 1;
 
 async function checkLastElement() {
-        if (!end) {
-            view.onscroll = '';
-            setTimeout(() => view.onscroll = checkLastElement, 20);
-            if ($('tbody tr:last').offset().top < $('#view').height() * 2) {
-                    array = await getAll(page);
-                    if (array == null || array.length < 25) {
-                        view.onscroll = '';
-                        end = true;
-                    }
-                    array.forEach(item => {
-                        let rown = document.createElement('tr');
-                        rown.id = `${item.ID}`;
-                        rown.onclick = function () {
-                            getType(this)
-                        };
-                        let data = new Array(4);
-                        rown.classList.add(`${item.IDENTIFIER}`);
-                        if (item.ID) {
-                            let rown_data_1 = document.createElement('td');
-                            let rown_data_2 = document.createElement('td');
-                            let rown_data_3 = document.createElement('td');
-                            let rown_data_4 = document.createElement('td');
-                            rown.appendChild(rown_data_1);
-                            rown.appendChild(rown_data_2);
-                            rown.appendChild(rown_data_3);
-                            rown.appendChild(rown_data_4);
-
-                            rown.firstChild.innerHTML = number;
-                            rown.children[find_header('Название')].innerHTML = `${item.NAME}`;
-                            rown.children[find_header('Тип подразделения')].innerHTML = `${item.DIVISION_TYPE_NAME}`;
-                            rown.children[find_header('Наименование')].innerHTML = `${item.TYPE_NAME}`;
-
-                            tbody.appendChild(rown);
-                            number++;
-                        }
-                    })
-                    page++;
-                }
+    if (!end) {
+        view.onscroll = '';
+        setTimeout(() => view.onscroll = checkLastElement, 20);
+        if ($('tbody tr:last').offset().top < $('#view').height() * 2) {
+            array = await getAll(page);
+            if (array == null || array.length < 25) {
+                view.onscroll = '';
+                end = true;
             }
-        }
+            const sequence = ['№', ...Array.prototype.slice.call(document.getElementsByClassName('drag_accept')).map((item) => item.innerHTML)]
 
-        function find_header(header) {
-            let id = 0;
-            for (let name of thead.firstChild.childNodes) {
-                if (name.innerHTML == header) {
-                    break;
-                }
-                id++;
-            }
-            return id;
+            array.forEach((item) => {
+                const row = tbody.appendChild(createElemWithAttr('tr', {
+                    id: item.ID,
+                    onclick: function () {
+                        getType(this)
+                    },
+                    className: item.IDENTIFIER
+                }));
+                sequence.forEach((title) => {
+                    row.insertCell().innerHTML = title === '№' ? number :
+                        title === 'Название' ? item.NAME :
+                        title === 'Тип подразделения' ? item.DIVISION_TYPE_NAME :
+                        title === 'Наименование' ? item.TYPE_NAME : '';
+                })
+                number++;
+            })
+            page++;
         }
+    }
+}
+
+function find_header(header) {
+    let id = 0;
+    for (let name of thead.firstChild.childNodes) {
+        if (name.innerHTML == header) {
+            break;
+        }
+        id++;
+    }
+    return id;
+}
