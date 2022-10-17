50bb5a439020df893ea473e240e70e3670031465
Xenia Kitlain <kitlainkseniya@gmail.com>
2022-10-17 20:41:42 +0700

    Merge conflicts

---
+++ js/bar.js
@@ -0,0 +1,9 @@
+function w3_open() {
+    document.getElementById("main").style.marginRight = "21%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("main").style.marginRight = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+}
