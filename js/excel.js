240690ba616c6d69f95e6c1aa6ca4680ac0feef0
ArhaTe <vova.kamashev_05@mail.ru>
2022-10-18 19:21:22 +0700

    fix

--- js/excel.js
+++ js/excel.js
@@ -20,6 +20,28 @@ const createFile = (function() {
     }
 })();
 
+async function createMockBody()
+{
+    let tbody = createElemWithAttr('tbody', {
+            id: 'tbody'
+        });
+    const sequence = ['№', ...Array.prototype.slice.call(document.getElementsByClassName('drag_accept')).map((item) => item.innerHTML)]
+    let num = 1;
+    data = await getAll('all');
+    data.forEach((item) => {
+            if (!isAllow(item)) return;
+            const row = tbody.appendChild(createElemWithAttr('tr', {
+            }))
+            sequence.forEach((title) => {
+                row.insertCell().innerHTML = title === '№' ? num :
+                    title === 'Название' ? item.NAME :
+                    title === 'Тип подразделения' ? item.DIVISION_TYPE_NAME :
+                    title === 'Наименование' ? item.TYPE_NAME : '';
+            })
+            num++})
+    return tbody;
+}
+                 
 async function toExcel() {
     if (view.classList.contains('search')) {
         exportation(document.getElementById('table'));
@@ -30,9 +52,9 @@ async function toExcel() {
         data = await cache('table', 'all');
         if (!data.length) return;
     }
-    page = 0; end = false; number = 1;
+    page = 0; end = false;
     
     const table = document.createElement('table');
-    $(table).append(createHead()); $(table).append(await createBody(data.length ? data : undefined));
+    $(table).append(createHead()); $(table).append(await createMockBody());
     createFile(table, `${document.getElementById('title').innerHTML}`, `${document.getElementById('title').innerHTML}.xlsx`);
 }
